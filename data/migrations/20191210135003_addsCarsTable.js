33146eb9d19196f781145be2371443a6fab5541f
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-12-10 15:18:54 -0500

    made migration and endpoints

--- data/migrations/20191210135003_addsCarsTable.js
+++ data/migrations/20191210135003_addsCarsTable.js
@@ -1,8 +1,21 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.string('vin', 255)
+            .unique()
+            .notNullable()
+        tbl.string('make', 255)
+            .notNullable()
+        tbl.string('model', 255)
+            .notNullable()
+        tbl.integer('milage')
+            .notNullabale()
+        tbl.string('transmission')
+        tbl.string('titleStatus')
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('cars');
 };
